f5a7125d74154580d7ea813a2dd19dfe3358e483
Chung-Chi-Lin <z0925955648@gmail.com>
2022-11-28 23:00:32 +0800

    first update

---
+++ javascripts/imgsChange.js
@@ -0,0 +1,114 @@
+const All = document.getElementById('All');
+const Allen = document.getElementById('Allen');
+const Sherry = document.getElementById('Sherry');
+const John = document.getElementById('John');
+
+const Imgs = document.getElementById('imgs');
+
+All.addEventListener('click',()=>{
+  Imgs.innerHTML=
+`                <div class="col">
+<div class="card text-white">
+  <img src="./images/dog05.png" class="card-img" alt="黃狗">
+  <div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+    <div class="content">
+      <small class="card-title f-kalam">designer</small>
+      <p class="card-text f-kalam fw-bold">Allen</p>
+    </div>
+  </div>
+</div>
+</div>
+<div class="col">
+<div class="card text-white">
+<img src="./images/dog06.png" class="card-img" alt="黑色狗">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">Sherry</p>
+  </div>
+</div>
+</div>
+</div>
+<div class="col">
+<div class="card text-white">
+<img src="./images/dog07.png" class="card-img" alt="白色橘點貓">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">John</p>
+  </div>
+</div>
+</div>
+</div>
+<div class="col pb-md-5">
+<div class="card text-white">
+<img src="./images/Michael.png" class="card-img" alt="白色狗">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">Ada</p>
+  </div>
+</div>
+</div>
+</div>`
+})
+
+Allen.addEventListener('click',()=>{
+  Imgs.innerHTML=
+`<div class="col py-4">
+  <div class="card text-white">
+    <img src="./images/dog05.png" class="card-img" alt="黃狗">
+    <div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+      <div class="content">
+        <small class="card-title f-kalam">designer</small>
+        <p class="card-text f-kalam fw-bold">Allen</p>
+      </div>
+    </div>
+  </div>
+</div>`
+});
+
+Sherry.addEventListener('click',()=>{
+  Imgs.innerHTML=
+`<div class="col py-4">
+<div class="card text-white">
+<img src="./images/dog06.png" class="card-img" alt="黑色狗">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">Sherry</p>
+  </div>
+</div>
+</div>
+</div>`
+});
+
+John.addEventListener('click',()=>{
+  Imgs.innerHTML=
+`<div class="col py-4">
+<div class="card text-white">
+<img src="./images/dog07.png" class="card-img" alt="白色橘點貓">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">John</p>
+  </div>
+</div>
+</div>
+</div>`
+});
+
+Ada.addEventListener('click',()=>{
+  Imgs.innerHTML=
+`<div class="col py-4">
+<div class="card text-white">
+<img src="./images/Michael.png" class="card-img" alt="白色狗">
+<div class="card-img-overlay bottom-auto bg-dark-gradient d-flex align-items-end">
+  <div class="content">
+    <small class="card-title f-kalam">designer</small>
+    <p class="card-text f-kalam fw-bold">Ada</p>
+  </div>
+</div>
+</div>
+</div>`
+});
